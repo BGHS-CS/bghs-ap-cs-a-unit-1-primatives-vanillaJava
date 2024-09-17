b3ef448bc318fa3f31222dac5f3ad8aa45d6dee1
RangerDan <dvhill@danlearnsstuff.com>
2024-09-17 07:56:42 -0700

    Adds test to Unit 1 Assignment

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-// Your code goes here! (Erase this line) 
+public class Main {
+    public static void main (String arg[]){
+        String unit = "Unit ";
+        double num = 2.0;
+        num -= 1.0;
+        System.out.println(unit+(int)num);   
+    }
+}
